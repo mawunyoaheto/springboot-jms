6094fb51ed2dc9183007930df1b94fb86a99dfba
Raymond Mawunyo Aheto <40844791+mawunyoaheto@users.noreply.github.com>
2022-06-12 17:17:45 -0500

    no commit

--- src/main/java/edu/miu/cs/cs544/raymond/springbootjms/SpringBootJmsApplication.java
+++ src/main/java/edu/miu/cs/cs544/raymond/springbootjms/SpringBootJmsApplication.java
@@ -10,7 +10,7 @@ public class SpringBootJmsApplication {
 
     public static void main(String[] args) {
         System.out.println("App start...");
-        System.out.println("App start...")
+        System.out.println("App start...");
         SpringApplication.run(SpringBootJmsApplication.class, args);
         System.out.println("App end...");
     }
