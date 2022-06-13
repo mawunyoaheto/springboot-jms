32f191a64b282623dea1f878ea58751a3d9cec4f
Raymond Mawunyo Aheto <40844791+mawunyoaheto@users.noreply.github.com>
2022-06-12 19:20:23 -0500

    update commit

--- src/main/java/edu/miu/cs/cs544/raymond/springbootjms/SpringBootJmsApplication.java
+++ src/main/java/edu/miu/cs/cs544/raymond/springbootjms/SpringBootJmsApplication.java
@@ -9,7 +9,7 @@ import org.springframework.jms.annotation.EnableJms;
 public class SpringBootJmsApplication {
 
     public static void main(String[] args) {
-        System.out.println("App start...");
+        System.out.println("App start..");
         SpringApplication.run(SpringBootJmsApplication.class, args);
         System.out.println("App end...");
     }
